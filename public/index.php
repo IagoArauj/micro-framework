22afa348c169e50f5bfde056bb971553b02750ee
IagoArauj <iagoaoliveira14@outlook.com>
2020-06-22 19:03:20 -0300

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Auto Loader created by Composer
+ */
+require_once __DIR__ . "/../vendor/autoload.php";
+
+/**
+ * Turning on the lights
+ */
+require_once __DIR__ . "/../bootstrap/bootstrap.php";
