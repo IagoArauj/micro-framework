e9e1ebe667f2b61cbe48109a065b9451f3403e0e
IagoArauj <iagoaoliveira14@outlook.com>
2020-08-07 19:49:36 -0300

    New function "newController"

--- core/Factory/ClassMaker.php
+++ core/Factory/ClassMaker.php
@@ -4,5 +4,21 @@ namespace Core\Factory;
 
 class ClassMaker
 {
-    
+    /**
+     * Return a new object controller from namespace App\Controllers\
+     * or false if something went wrong
+     * 
+     * @param string $class
+     * @return mixed
+     */
+    public static function newController(string $class)
+    {
+        try {
+            $class = 'App\\Controllers\\' . $class;
+            $obj = new $class;
+        } catch (\Throwable $th) {
+            $obj = false;
+        }
+        return $obj;
+    }
 }
