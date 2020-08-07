a5cf828df5171a54e5e8622acbbfc94c7b663427
IagoArauj <iagoaoliveira14@outlook.com>
2020-08-07 19:54:04 -0300

    new function "returnClosure"

--- core/Factory/ClosureMaker.php
+++ core/Factory/ClosureMaker.php
@@ -3,6 +3,19 @@
 namespace Core\Factory;
 
 class ClosureMaker
-{
-    
+{   
+    /**
+     * Return the closure given or false if something went wrong
+     * 
+     * @param callable $closure
+     * @return mixed
+     */
+    public static function returnClosure(callable $closure)
+    {
+        try {
+            return call_user_func($closure);
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
 }
