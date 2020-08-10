2682480e7db208aed626eccfacb0b3607f69d7e0
IagoArauj <iagoaoliveira14@outlook.com>
2020-08-10 17:07:13 -0300

    Router is now in Utils namespace

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+use Core\Utils\Router;
+
 include_once __DIR__ . '/../routes/route.php';
 // Turn on the router
-Core\Routing\Router::on();
+Router::on();
--- routes/route.php
+++ routes/route.php
@@ -1,6 +1,6 @@
 <?php
 
-use Core\Routing\Router;
+use Core\Utils\Router;
 
 /**
  * In this file you can register all routes for the app.
